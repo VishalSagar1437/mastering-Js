639a5dab6a4fb2223791922521ab83f5d2f848af
Vishal Sagar <96104706+VishalSagar1437@users.noreply.github.com>
2025-02-02 14:51:35 +0000

    Conversions of Datatypes

---
+++ 01_Basics/03-ConversionsOperations.js
@@ -0,0 +1,32 @@
+console.log("Radhe Radhe");
+
+// let score = true
+
+// console.log(typeof score);
+
+// let val  = Number(score);
+// console.log(typeof val);
+
+// console.log(val);
+// // console.log(score);
+
+// console.log(val);
+
+
+
+
+
+// let score = "dlnkjdn";
+
+// let val = Boolean(score);
+// console.log(val);
+
+
+// 1 => true 0 => false
+//"" => false "radhe" => true
+
+
+let num = 23
+
+let str = String(num);
+console.log(typeof str);
