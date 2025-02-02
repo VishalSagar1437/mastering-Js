7c8e18deeb849149525db4879ab9f937dc91d24c
Vishal Sagar <96104706+VishalSagar1437@users.noreply.github.com>
2025-02-02 13:00:28 +0000

    variables and Datatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+let Email = "vishas@dmd.com"
+const pass = "2321"
+var id = "dkndkn"
+// pass = "jnjd"    ASSIGNMENT TO CONSTANT VARIABLE
+
+
+// PREFER NOT TO USE VAR BCOZ OF FUNCTIONAL AND BLOCK SCOPE
+
+// console.log(pass);
+let con;
+let str = null;
+console.table([Email,pass,id,con,str])
---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,6 @@
+"use Strict"; // Treat all Js Code as newer version
+console.log("Radhe Radhe");
+
+// alert("Not applicable"); Executable in browser not NodeJs
+
+
