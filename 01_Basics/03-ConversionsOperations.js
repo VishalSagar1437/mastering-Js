07497de6e1866d2ff3a854595030d1233e296e15
Vishal Sagar <96104706+VishalSagar1437@users.noreply.github.com>
2025-02-06 12:22:42 +0000

    conversions and comparisons completed

--- 01_Basics/03-ConversionsOperations.js
+++ 01_Basics/03-ConversionsOperations.js
@@ -29,4 +29,59 @@ console.log("Radhe Radhe");
 let num = 23
 
 let str = String(num);
-console.log(typeof str);
+// console.log(typeof str);
+
+
+
+// OPERATIONS //
+
+let val = 3;
+let nega = -val;
+// console.log(nega);
+
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1 = "Radhe"
+let str2 = " Krishna"
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+
+// console.log("1" + 2 +3);
+// console.log(2+3+"1")
+
+// console.log(+true);
+// console.log(+"");
+
+
+
+// let cnt = 99;
+// ++cnt;
+// console.log(cnt);
+
+// let count = 99;
+
+// console.log(count);
+
+
+// console.log("2" > 1); // 2 gets converted to a number automatically
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0); // Null gets conv to a num treated it as a 0 
+
+
+// console.log("2" ==2);// yaha conv hota h string to num
+// console.log("2" ===2);// this is a strict way of checking and this also checks if the datatype is also same or not
+
+
+
+
+
+
---
+++ 01_Basics/04_Comaprisons.js
@@ -0,0 +1,11 @@
+// console.log("2" > 1); // 2 gets converted to a number automatically
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0); // Null gets conv to a num treated it as a 0 
+
+
+// console.log("2" ==2);// yaha conv hota h string to num
+// console.log("2" ===2);// this is a strict way of checking and this also checks if the datatype is also same or not
+
+
