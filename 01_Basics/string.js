fcc298b989eddf0b63e31b51516f8b96c6aa8d8c
Vishal Sagar <96104706+VishalSagar1437@users.noreply.github.com>
2025-03-03 06:10:11 +0000

    strings

---
+++ 01_Basics/string.js
@@ -0,0 +1,20 @@
+console.log("Radhe radhe");
+
+const gName = new String("Vishal");
+
+// console.log(typeof(gName));
+// console.log(gName[0]);
+// console.log(gName.__proto__);
+// console.log(gName.length);
+
+// console.log(gName.toLowerCase());
+// console.log(gName);
+// console.log(gName.charAt(2));
+// console.log(gName.indexOf('h'));
+
+const newString = gName.substring(0,4);
+ console.log(newString);
+ 
+ const anoString = gName.slice(-5,4);
+ console.log(anoString);
+ 
