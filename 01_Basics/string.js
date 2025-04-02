e3da0f0e6c1cc4568aa90aac372d74d56d9134d4
Vishal Sagar <96104706+VishalSagar1437@users.noreply.github.com>
2025-04-02 10:38:46 +0000

    maths ch6 done

--- 01_Basics/string.js
+++ 01_Basics/string.js
@@ -12,9 +12,24 @@ const gName = new String("Vishal");
 // console.log(gName.charAt(2));
 // console.log(gName.indexOf('h'));
 
-const newString = gName.substring(0,4);
- console.log(newString);
+// const newString = gName.substring(0,4);
+//  console.log(newString);
  
- const anoString = gName.slice(-5,4);
- console.log(anoString);
+//  const anoString = gName.slice(-5,4);
+//  console.log(anoString);
+
+// const url = "vishal#32@gmail.com";
+// console.log(url.replace('#','1')); // replaces the # with 1 
+
+// const str = "     vishal         ";
+// console.log(str);
+
+// console.log(str.trimStart());
+
+const str = "there was a king in the country";
+
+console.log(str.split(' '));
+
+
+ 
  
